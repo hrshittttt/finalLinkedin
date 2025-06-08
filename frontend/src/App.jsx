f43d2ed440d40e1833da198834446a8cae4694ef
Harshit Suthar <harshitsuthar9087@icloud.com>
2025-06-09 03:38:57 +0530

    AI se JobLens

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,12 @@ import Login from "../components/Authentication/Login.jsx";
 import Header from "./Header.jsx";
 import Hero from "../components/Main/Hero.jsx";
 import Signup from "../components/Authentication/Signup.jsx";
+import Lens from "../components/JobLens/Lens.jsx";
 
 export default function App() {
   return (
     <>
-      <Header />
-      <Hero />
+      <Lens />
     </>
   );
 }
